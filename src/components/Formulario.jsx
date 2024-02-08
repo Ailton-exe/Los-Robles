0642817638c694f1ac94776698a331351d07a9af
Ailton-exe <ailton.ramos23b@udgvirtual.udg.mx>
2024-02-07 21:11:24 -0600

    Login

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,54 @@
+import "./Formulario.css"
+import { useState } from "react"
+
+export function Formulario ({setUser}) {
+    const [nombre, setNombre] = useState("")
+    const [contraseña, setContraseña] = useState("")
+    const [error, setError] = useState(false)
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+
+        if (nombre === "" || contraseña === "") {
+            setError(true)
+            return
+        }
+
+        setError(false)
+
+        setUser(nombre)
+        setUser([nombre])
+    }
+    
+
+    return(
+        <section>
+            <h1>Los Robles</h1>
+
+            <form 
+            className="formulario"
+            onSubmit={handleSubmit}
+            >
+                <input type="text" 
+                placeholder="correo@ejemplo.com"
+                value={nombre}
+                onChange={e => setNombre(e.target.value)}
+                />
+
+
+                <input 
+                type="password" 
+                placeholder="*******" 
+                value={contraseña}
+                onChange={e => setContraseña(e.target.value)}
+                />
+
+
+                <button>Iniciar sesión</button>
+            </form>
+
+            {error && <p>Todos los campos son obligatorios</p>}
+
+        </section>
+    )
+}
